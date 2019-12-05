e0f74cc680c8d40965a598d1e48397c2e08c4d25
yanming1560 <36906575+yanming1560@users.noreply.github.com>
2019-12-05 15:03:38 +0800

    Update deal_image.py

--- deal_image.py
+++ deal_image.py
@@ -7,7 +7,7 @@ part = im[0:1080, 0:1920]  # 裁图
 # cap = cv2.VideoCapture(0)
 x,y=pag.position()
 num=1
-while(not(x<=1 and y<=1)):
+while(not(x<=1 and y<=1)):  #结束判断，可以截图触发结束,也可以判断鼠标位置不截图退出。
     # cv2.imshow('suofanghou',suofanghou)
     # cv2.imshow('im',im)
     # cv2.imshow('part',part)
@@ -20,8 +20,15 @@ while(not(x<=1 and y<=1)):
     x,y=pag.position()
     part2=cv2.rectangle(part,(x,y),(x+500,y+500),(255,255,255),2)
     cv2.imshow('part2', part2)
-    if cv2.waitKey(1) & 0xFF == ord('p'):
+    if cv2.waitKey(1) & 0xFF == ord('p'):       #只能用一个，两个就不好使了。
         cv2.imwrite("cut_%s.jpg"%num,im[y:y+500,x:x+500])
         num+=1
+        # break
+
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+
+
 
 cv2.destroyAllWindows()
+
